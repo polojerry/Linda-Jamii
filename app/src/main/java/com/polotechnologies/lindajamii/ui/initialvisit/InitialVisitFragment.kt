814bf70315c4663608773965ec891890928800e3
polojerry <polojerry4@gmail.com>
2020-04-30 01:07:06 +0300

    Updated fragment_delivery.xml, fragment_initial_visit.xml, fragment_post_natal_visit.xml, fragment_subsequent_visits.xml to use data binding

--- app/src/main/java/com/polotechnologies/lindajamii/ui/initialvisit/InitialVisitFragment.kt
+++ app/src/main/java/com/polotechnologies/lindajamii/ui/initialvisit/InitialVisitFragment.kt
@@ -5,19 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.polotechnologies.lindajamii.R
+import com.polotechnologies.lindajamii.databinding.FragmentInitialVisitBinding
 
 /**
  * A fragment used to insert data of initial visit by the expectant mother
  */
 class InitialVisitFragment : Fragment() {
 
+    private lateinit var mBinding: FragmentInitialVisitBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_initial_visit, container, false)
+        mBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_initial_visit, container, false )
+        return mBinding.root
     }
 
 }

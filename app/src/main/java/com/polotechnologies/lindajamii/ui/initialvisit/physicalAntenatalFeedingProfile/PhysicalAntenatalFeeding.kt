248cc7d8d58bcb31bf03997583d914d792cbbca1
polojerry <polojerry4@gmail.com>
2020-05-01 21:06:43 +0300

    Created PhysicalAntenatalFeeding.kt to store and insert data on mothers physical examination. antenatal profile and Infant feeding. Design Done

---
+++ app/src/main/java/com/polotechnologies/lindajamii/ui/initialvisit/physicalAntenatalFeedingProfile/PhysicalAntenatalFeeding.kt
@@ -0,0 +1,23 @@
+package com.polotechnologies.lindajamii.ui.initialvisit.physicalAntenatalFeedingProfile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.polotechnologies.lindajamii.R
+
+/**
+ *
+ */
+class PhysicalAntenatalFeeding : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_physical_antenatal_feeding, container, false)
+    }
+
+}

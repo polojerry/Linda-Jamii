68181591f628a2846c43f47048f793a6f0c1a966
polojerry <polojerry4@gmail.com>
2020-05-02 15:14:29 +0300

    Subsequent Visit Data Entry Setup

--- app/src/main/java/com/polotechnologies/lindajamii/ui/subsequentVisits/SubsequentVisitViewModel.kt
+++ app/src/main/java/com/polotechnologies/lindajamii/ui/subsequentVisits/SubsequentVisitViewModel.kt
@@ -1,82 +1,189 @@
 package com.polotechnologies.lindajamii.ui.subsequentVisits
 
-import android.app.Application
-import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat
-import androidx.appcompat.widget.AppCompatImageView
-import androidx.lifecycle.AndroidViewModel
+
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.polotechnologies.lindajamii.dataModels.PatientDetails
-import com.polotechnologies.lindajamii.dataModels.PatientDetails.*
-import com.polotechnologies.lindajamii.database.PatientProfileDAO
-import kotlinx.coroutines.*
-
-class SubsequentVisitViewModel(val app: Application, val database: PatientProfileDAO) :
-    AndroidViewModel(app) {
-
-    private val _maternalProfile = MutableLiveData<MaternalProfile>()
-    val maternalProfile: LiveData<MaternalProfile>
-        get() = _maternalProfile
-
-    private val _medicalSurgicalHistory = MutableLiveData<MedicalSurgicalHistory>()
-    val medicalSurgicalHistory: LiveData<MedicalSurgicalHistory>
-        get() = _medicalSurgicalHistory
-
-    private val _physicalExamination = MutableLiveData<PhysicalExamination>()
-    val physicalExamination: LiveData<PhysicalExamination>
-        get() = _physicalExamination
-
-    private val _antenatalProfile = MutableLiveData<AntenatalProfile>()
-    val antenatalProfile: LiveData<AntenatalProfile>
-        get() = _antenatalProfile
+import androidx.lifecycle.ViewModel
+import com.google.firebase.firestore.FirebaseFirestore
+import com.polotechnologies.lindajamii.dataModels.ExpectantDetails
+import com.polotechnologies.lindajamii.dataModels.ExpectantSubsequentVisit
+import com.polotechnologies.lindajamii.dataModels.SubsequentVisit
+import com.polotechnologies.lindajamii.databinding.FragmentSubsequentVisitsBinding
 
 
+class SubsequentVisitViewModel(val mDatabase: FirebaseFirestore, val mBinding: FragmentSubsequentVisitsBinding) :
+    ViewModel() {
 
-    //Inserted Profile ID
-    private val _insertedDetailId = MutableLiveData<Long>()
-    val insertedDetailId: LiveData<Long>
-        get() = _insertedDetailId
+    private val _writeStatus = MutableLiveData<Boolean>()
+    val writeStatus: LiveData<Boolean>
+        get() = _writeStatus
 
-    private var viewModelJob = Job()
-    private val coroutineScope = CoroutineScope(viewModelJob + Dispatchers.Main)
+    private val _exception = MutableLiveData<Exception>()
+    val exception: LiveData<Exception>
+        get() = _exception
+
+    private var registrationNumber = ""
+    private var numberOfVisit = ""
+    private var dateOfVisit = ""
+    private var urine = ""
+    private var weight = ""
+    private var bp = ""
+    private var hb = ""
+    private var pallor = ""
+    private var maturity = ""
+    private var fundalHeight = ""
+    private var presentation = ""
+    private var lie = ""
+    private var foetalHeart = ""
+    private var foetalMovement = ""
+    private var nextVisit = ""
 
 
-    fun saveMaternalProfile(maternalProfile: MaternalProfile) {
-        _maternalProfile.value = maternalProfile
-    }
-    fun saveMedicalSurgicalHistory(medicalSurgicalHistory: MedicalSurgicalHistory) {
-        _medicalSurgicalHistory.value = medicalSurgicalHistory
-    }
-    fun savePhysicalExamination(physicalExamination: PhysicalExamination) {
-        _physicalExamination.value = physicalExamination
-    }
-    fun saveAntenatalProfile(antenatalProfile: AntenatalProfile) {
-        _antenatalProfile.value = antenatalProfile
-    }
+    fun isFieldsValid(): Boolean {
+        var isValid = false
 
-    fun savePatientProfile(){
-        coroutineScope.launch {
-            insertPatientToDatabase()
+        registrationNumber = mBinding.textSubsequentVisitsRegistrationNumber.text.toString()
+        numberOfVisit = mBinding.textSubsequentVisitsNumberOfVisit.text.toString()
+        dateOfVisit = mBinding.textSubsequentVisitsDate.text.toString()
+        urine = mBinding.textSubsequentVisitsUrine.text.toString()
+        weight = mBinding.textSubsequentVisitsWeight.text.toString()
+        bp = mBinding.textSubsequentVisitsBp.text.toString()
+        hb = mBinding.textSubsequentVisitsHb.text.toString()
+        pallor = mBinding.textSubsequentVisitsPallor.text.toString()
+        maturity = mBinding.textSubsequentVisitsMaturity.text.toString()
+        fundalHeight = mBinding.textSubsequentVisitsFundalHeight.text.toString()
+        presentation = mBinding.textSubsequentVisitsPresentation.text.toString()
+        lie = mBinding.textSubsequentVisitsLie.text.toString()
+        foetalHeart = mBinding.textSubsequentVisitsFoetalHeart.text.toString()
+        foetalMovement = mBinding.textSubsequentVisitsFoetalMvt.text.toString()
+        nextVisit = mBinding.textSubsequentVisitsNextVisit.text.toString()
+
+
+        if (numberOfVisit == "") {
+            isValid = false
+            mBinding.textLayoutSubsequentVisitsRegistrationNumber.error = "Required"
         }
+
+        if (numberOfVisit == "") {
+            isValid = false
+            mBinding.textLayoutSubsequentVisitsNumberOfVisit.error = "Required"
+        }
+
+        if (dateOfVisit == "") {
+            isValid = false
+            mBinding.textLayoutSubsequentVisitsDate.error = "Required"
+        }
+        if (urine == "") {
+            isValid = false
+            mBinding.textLayoutSubsequentVisitsUrine.error = "Required"
+        }
+        if (weight == "") {
+            isValid = false
+            mBinding.textLayoutSubsequentVisitsWeight.error = "Required"
+        }
+        if (bp == "") {
+            mBinding.textLayoutSubsequentVisitsBp.error = "Required"
+            isValid = false
+        }
+        if (hb == "") {
+            isValid = false
+            mBinding.textLayoutSubsequentVisitsHb.error = "Required"
+        }
+        if (pallor == "") {
+            isValid = false
+            mBinding.textLayoutSubsequentVisitsPallor.error = "Required"
+        }
+
+        if (maturity == "") {
+            isValid = false
+            mBinding.textLayoutSubsequentVisitsMaturity.error = "Required"
+        }
+
+        if (fundalHeight == "") {
+            isValid = false
+            mBinding.textLayoutSubsequentVisitsFundalHeight.error = "Required"
+        }
+        if (presentation == "") {
+            isValid = false
+            mBinding.textLayoutSubsequentVisitsPresentation.error = "Required"
+        }
+        if (lie == "") {
+            isValid = false
+            mBinding.textLayoutSubsequentVisitsLie.error = "Required"
+        }
+        if (foetalHeart == "") {
+            isValid = false
+            mBinding.textLayoutSubsequentFoetalHeart.error = "Required"
+        }
+        if (foetalMovement == "") {
+            isValid = false
+            mBinding.textLayoutSubsequentVisitsFoetalMvt.error = "Required"
+        }
+
+        if (nextVisit == "") {
+            isValid = false
+            mBinding.textLayoutSubsequentNextVisit.error = "Required"
+        }
+
+        registrationNumber = mBinding.textSubsequentVisitsRegistrationNumber.text.toString()
+        numberOfVisit = mBinding.textSubsequentVisitsNumberOfVisit.text.toString()
+        dateOfVisit = mBinding.textSubsequentVisitsDate.text.toString()
+        urine = mBinding.textSubsequentVisitsUrine.text.toString()
+        weight = mBinding.textSubsequentVisitsWeight.text.toString()
+        bp = mBinding.textSubsequentVisitsBp.text.toString()
+        hb = mBinding.textSubsequentVisitsHb.text.toString()
+        pallor = mBinding.textSubsequentVisitsPallor.text.toString()
+        maturity = mBinding.textSubsequentVisitsMaturity.text.toString()
+        fundalHeight = mBinding.textSubsequentVisitsFundalHeight.text.toString()
+        presentation = mBinding.textSubsequentVisitsPresentation.text.toString()
+        lie = mBinding.textSubsequentVisitsLie.text.toString()
+        foetalHeart = mBinding.textSubsequentVisitsFoetalHeart.text.toString()
+        foetalMovement = mBinding.textSubsequentVisitsFoetalMvt.text.toString()
+        nextVisit = mBinding.textSubsequentVisitsNextVisit.text.toString()
+
+
+        if (registrationNumber != "" && numberOfVisit != "" && dateOfVisit != "" && urine != "" && weight != ""
+            && bp != "" && hb != "" && pallor != "" && maturity != "" && fundalHeight != ""
+            && presentation != "" && lie != "" && foetalHeart != ""&& presentation != "" && foetalMovement != "" && nextVisit != ""
+        ) {
+            isValid = true
+        }
+
+        return isValid
+
     }
 
-    private suspend fun insertPatientToDatabase() {
-
-        val patientProfile = PatientDetails(
-            0L,
-            _maternalProfile.value,
-            _medicalSurgicalHistory.value,
-            _physicalExamination.value,
-            _antenatalProfile.value
+    fun saveMedicalSurgicalHistory() {
+        val subsequentVisit = ExpectantSubsequentVisit(
+            registrationNumber,
+            numberOfVisit,
+            dateOfVisit,
+            urine,
+            weight,
+            bp,
+            hb ,
+            pallor,
+            maturity,
+            fundalHeight,
+            presentation,
+            lie ,
+            foetalHeart,
+            foetalMovement,
+            nextVisit
         )
-        withContext(Dispatchers.IO){
-            _insertedDetailId.value = database.insert(patientProfile)
+
+        mDatabase.collection("patients")
+            .document("subsequentVisits")
+            .collection(registrationNumber).add(subsequentVisit
+        ).addOnSuccessListener {
+            _writeStatus.value = true
+        }.addOnFailureListener { exception ->
+            _exception.value = exception
+            _writeStatus.value = false
+
         }
+
     }
 
-
-    override fun onCleared() {
-        viewModelJob.cancel()
-    }
 
 }
--- app/src/main/java/com/polotechnologies/lindajamii/ui/subsequentVisits/SubsequentVisitViewModelFactory.kt
+++ app/src/main/java/com/polotechnologies/lindajamii/ui/subsequentVisits/SubsequentVisitViewModelFactory.kt
@@ -1,30 +1,29 @@
-/*
 package com.polotechnologies.lindajamii.ui.subsequentVisits
 
 import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.google.firebase.firestore.FirebaseFirestore
 import com.polotechnologies.lindajamii.database.PatientProfileDAO
-import com.polotechnologies.lindajamii.database.SubsequentDAO
+import com.polotechnologies.lindajamii.databinding.FragmentSubsequentVisitsBinding
 
-*/
 /**
  * Simple ViewModel factory for Initial Visit ViewModel
- *//*
+ */
 
 class SubsequentVisitViewModelFactory(
-    private val application: Application,
-    private val database: SubsequentDAO
+    private val mDatabase: FirebaseFirestore,
+    val mBinding: FragmentSubsequentVisitsBinding
 ) : ViewModelProvider.Factory {
     @Suppress("unchecked_cast")
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(InitialVisitViewModel::class.java)) {
-            return InitialVisitViewModel(
-                application,
-                database
+        if (modelClass.isAssignableFrom(SubsequentVisitViewModel::class.java)) {
+            return SubsequentVisitViewModel(
+                mDatabase,
+                mBinding
             ) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
 }
-*/
+

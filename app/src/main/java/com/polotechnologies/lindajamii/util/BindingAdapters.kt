96fde853bb4eb186ba0ff047923064cc504dd879
polojerry <polojerry4@gmail.com>
2020-05-15 07:52:03 +0300

    Added Check to see if nextVisitDate has been passed, changing the background color

--- app/src/main/java/com/polotechnologies/lindajamii/util/BindingAdapters.kt
+++ app/src/main/java/com/polotechnologies/lindajamii/util/BindingAdapters.kt
@@ -1,23 +1,38 @@
 package com.polotechnologies.lindajamii.util
 
 import android.text.format.DateFormat
-import android.util.Log
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.databinding.BindingAdapter
+import com.google.android.material.card.MaterialCardView
+import com.polotechnologies.lindajamii.R
 import com.polotechnologies.lindajamii.dataModels.ExpectantDetails
 import java.util.Calendar
-import java.util.Locale
 
 @BindingAdapter("nextVisit")
 fun bind(textView: AppCompatTextView, expectantDetails: ExpectantDetails){
     expectantDetails.nextVisit.let {nextVisit->
-        
         if(nextVisit!=null){
             val calender = Calendar.getInstance()
             calender.timeInMillis = nextVisit
 
             val nextVisitDate = DateFormat.format("dd/MM/yy", calender).toString()
             textView.text = nextVisitDate
+        }
+
+    }
+
+}
+
+
+@BindingAdapter("visitBackground")
+fun bind(expectantCard: MaterialCardView, expectantDetails: ExpectantDetails){
+    expectantDetails.nextVisit.let {nextVisit->
+        if(nextVisit!=null){
+            val dateToday = Calendar.getInstance().timeInMillis
+
+            if(dateToday > nextVisit){
+                expectantCard.setBackgroundColor(R.color.colorLightRed)
+            }
         }
 
     }

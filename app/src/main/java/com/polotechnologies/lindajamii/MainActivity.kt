502a72b4ec53431a317f70f0a89db631aa6f62ee
polojerry <polojerry4@gmail.com>
2020-06-20 22:13:15 +0300

    Added SignOut of user

--- app/src/main/java/com/polotechnologies/lindajamii/MainActivity.kt
+++ app/src/main/java/com/polotechnologies/lindajamii/MainActivity.kt
@@ -6,6 +6,8 @@ import android.content.Context
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
@@ -19,18 +21,20 @@ import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
 import com.polotechnologies.lindajamii.databinding.ActivityMainBinding
 import com.polotechnologies.lindajamii.util.ExpectantVisitNotification
 
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener,
-    Toolbar.OnMenuItemClickListener {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private lateinit var mBinding: ActivityMainBinding
     private lateinit var navController: NavController
+    private lateinit var mAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_LindaJamii)
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        mAuth = FirebaseAuth.getInstance()
 
         setNavControllerOptions()
         registerNotificationChannel()
@@ -38,14 +42,16 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     }
 
     private fun registerNotificationChannel() {
-       if(Build.VERSION.SDK_INT > Build.VERSION_CODES.O){
-           val nm = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-           val channel  = NotificationChannel(ExpectantVisitNotification.NOTIFICATION_CHANNEL,
-               "Linda Jamii",
-               NotificationManager.IMPORTANCE_HIGH)
-           nm.createNotificationChannel(channel)
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
+            val nm = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            val channel = NotificationChannel(
+                ExpectantVisitNotification.NOTIFICATION_CHANNEL,
+                "Linda Jamii",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            nm.createNotificationChannel(channel)
 
-       }
+        }
     }
 
     private fun setNavControllerOptions() {
@@ -58,7 +64,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                     mBinding.toolbarMain.visibility = View.GONE
                     mBinding.drawerLayoutMain.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
                 }
-                R.id.patientsDetailsFragment->{
+                R.id.patientsDetailsFragment -> {
                     mBinding.appBarMain.visibility = View.GONE
                     mBinding.toolbarMain.visibility = View.GONE
                 }
@@ -79,8 +85,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         mBinding.toolbarMain.visibility = View.VISIBLE
         mBinding.toolbarMain.title = destination.label
 
-        navController.graph.startDestination = R.id.homeFragment
-
         setSupportActionBar(mBinding.toolbarMain)
         supportActionBar!!.setDisplayShowHomeEnabled(true)
 
@@ -95,32 +99,32 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
 
-        if(navController.currentDestination!!.id == R.id.homeFragment){
-            when(item.itemId){
-                R.id.action_initial_visit->{
+        if (navController.currentDestination!!.id == R.id.homeFragment) {
+            when (item.itemId) {
+                R.id.action_initial_visit -> {
                     navController.navigate(R.id.action_homeFragment_to_initialVisitFragment)
                     mBinding.drawerLayoutMain.closeDrawers()
                 }
-                R.id.action_subsequent_visit->{
+                R.id.action_subsequent_visit -> {
                     navController.navigate(R.id.action_homeFragment_to_subsequentVisitsFragment)
                     mBinding.drawerLayoutMain.closeDrawers()
                 }
 
-                R.id.action_delivery->{
+                R.id.action_delivery -> {
                     navController.navigate(R.id.action_homeFragment_to_deliveryFragment)
                     mBinding.drawerLayoutMain.closeDrawers()
                 }
 
-                R.id.action_post_natal_visit->{
+                R.id.action_post_natal_visit -> {
                     navController.navigate(R.id.action_homeFragment_to_postNatalVisitFragment)
                     mBinding.drawerLayoutMain.closeDrawers()
                 }
 
-                R.id.action_patients->{
+                R.id.action_patients -> {
                     navController.navigate(R.id.action_homeFragment_to_patientsFragment)
                     mBinding.drawerLayoutMain.closeDrawers()
                 }
-                else->{
+                else -> {
                     return true
                 }
             }
@@ -129,10 +133,11 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         return true
     }
 
-    private fun checkCloseDrawer(){
-        /*item.isisChecked = true*/
-        mBinding.drawerLayoutMain.closeDrawers()
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater.inflate(R.menu.menu_main, menu)
+        return true
     }
+
 
     override fun onSupportNavigateUp(): Boolean {
         return NavigationUI.navigateUp(navController, mBinding.drawerLayoutMain)
@@ -147,10 +152,24 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
     }
 
-    override fun onMenuItemClick(item: MenuItem?): Boolean {
-        if (item!!.itemId == R.id.action_sign_out) {
-            Toast.makeText(applicationContext, "Signed Out Successfully", Toast.LENGTH_SHORT).show()
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_sign_out -> {
+                signOutUser()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
-        return true
+
+    }
+
+    private fun signOutUser() {
+        mAuth.signOut()
+        Toast.makeText(
+            baseContext.applicationContext,
+            "Signed Out Successfully",
+            Toast.LENGTH_SHORT
+        ).show()
+        findNavController(R.id.nav_host_main).navigate(R.id.homeFragment)
     }
 }
---
+++ app/src/main/java/com/polotechnologies/lindajamii/network/Resource.kt
@@ -0,0 +1,4 @@
+package com.polotechnologies.lindajamii.network
+
+import com.polotechnologies.lindajamii.dataModels.ExpectantDetails
+

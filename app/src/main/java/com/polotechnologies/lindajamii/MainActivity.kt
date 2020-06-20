8aeba493bb18c5c08198ae5c3bd915357ee7187e
polojerry <polojerry4@gmail.com>
2020-06-20 23:08:25 +0300

    Updated Home Display, Configured Navigation

--- app/src/main/java/com/polotechnologies/lindajamii/MainActivity.kt
+++ app/src/main/java/com/polotechnologies/lindajamii/MainActivity.kt
@@ -7,19 +7,18 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
-import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
-import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
-import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.polotechnologies.lindajamii.databinding.ActivityMainBinding
@@ -28,16 +27,32 @@ import com.polotechnologies.lindajamii.util.ExpectantVisitNotification
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private lateinit var mBinding: ActivityMainBinding
-    private lateinit var navController: NavController
+    private lateinit var mNavController: NavController
     private lateinit var mAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_LindaJamii)
+
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        mNavController = findNavController(R.id.nav_host_main)
+
         mAuth = FirebaseAuth.getInstance()
 
+        setUpToolBarAndDrawer()
         setNavControllerOptions()
+
+
         registerNotificationChannel()
+
+    }
+
+    private fun setUpToolBarAndDrawer() {
+        val appBarConfiguration = AppBarConfiguration(mNavController.graph, mBinding.drawerLayoutMain)
+        mBinding.toolbarMain.setupWithNavController(mNavController, appBarConfiguration)
+
+        mBinding.navigationViewMain.setupWithNavController(mNavController)
+        mBinding.navigationViewMain.setNavigationItemSelectedListener(this)
+        mBinding.drawerLayoutMain.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
 
     }
 
@@ -55,24 +70,17 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     }
 
     private fun setNavControllerOptions() {
-        navController =
-            supportFragmentManager.findFragmentById(R.id.nav_host_main)!!.findNavController()
-
-        navController.addOnDestinationChangedListener { _, destination, _ ->
+        mNavController.addOnDestinationChangedListener { _, destination, _ ->
             when (destination.id) {
                 R.id.loginFragment -> {
                     mBinding.toolbarMain.visibility = View.GONE
-                    mBinding.drawerLayoutMain.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
                 }
                 R.id.patientsDetailsFragment -> {
-                    mBinding.appBarMain.visibility = View.GONE
                     mBinding.toolbarMain.visibility = View.GONE
                 }
                 else -> {
-                    mBinding.appBarMain.visibility = View.VISIBLE
                     mBinding.toolbarMain.visibility = View.VISIBLE
-                    setUpToolbar(destination)
-                    setDrawerLayout()
+                    mBinding.drawerLayoutMain.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
 
                 }
 
@@ -81,47 +89,31 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         }
     }
 
-    private fun setUpToolbar(destination: NavDestination) {
-        mBinding.toolbarMain.visibility = View.VISIBLE
-        mBinding.toolbarMain.title = destination.label
-
-        setSupportActionBar(mBinding.toolbarMain)
-        supportActionBar!!.setDisplayShowHomeEnabled(true)
-
-        NavigationUI.setupActionBarWithNavController(this, navController, mBinding.drawerLayoutMain)
-    }
-
-    private fun setDrawerLayout() {
-        mBinding.drawerLayoutMain.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
-        NavigationUI.setupWithNavController(mBinding.navigationMain, navController)
-        mBinding.navigationMain.setNavigationItemSelectedListener(this)
-    }
-
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
 
-        if (navController.currentDestination!!.id == R.id.homeFragment) {
+        if (mNavController.currentDestination!!.id == R.id.homeFragment) {
             when (item.itemId) {
                 R.id.action_initial_visit -> {
-                    navController.navigate(R.id.action_homeFragment_to_initialVisitFragment)
+                    mNavController.navigate(R.id.action_homeFragment_to_initialVisitFragment)
                     mBinding.drawerLayoutMain.closeDrawers()
                 }
                 R.id.action_subsequent_visit -> {
-                    navController.navigate(R.id.action_homeFragment_to_subsequentVisitsFragment)
+                    mNavController.navigate(R.id.action_homeFragment_to_subsequentVisitsFragment)
                     mBinding.drawerLayoutMain.closeDrawers()
                 }
 
                 R.id.action_delivery -> {
-                    navController.navigate(R.id.action_homeFragment_to_deliveryFragment)
+                    mNavController.navigate(R.id.action_homeFragment_to_deliveryFragment)
                     mBinding.drawerLayoutMain.closeDrawers()
                 }
 
                 R.id.action_post_natal_visit -> {
-                    navController.navigate(R.id.action_homeFragment_to_postNatalVisitFragment)
+                    mNavController.navigate(R.id.action_homeFragment_to_postNatalVisitFragment)
                     mBinding.drawerLayoutMain.closeDrawers()
                 }
 
                 R.id.action_patients -> {
-                    navController.navigate(R.id.action_homeFragment_to_patientsFragment)
+                    mNavController.navigate(R.id.action_homeFragment_to_patientsFragment)
                     mBinding.drawerLayoutMain.closeDrawers()
                 }
                 else -> {
@@ -138,10 +130,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         return true
     }
 
-
-    override fun onSupportNavigateUp(): Boolean {
-        return NavigationUI.navigateUp(navController, mBinding.drawerLayoutMain)
-    }
 
     override fun onBackPressed() {
         if (mBinding.drawerLayoutMain.isDrawerOpen(GravityCompat.START)) {
